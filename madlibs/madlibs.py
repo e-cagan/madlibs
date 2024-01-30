207e5123ea49003066f614e493a74998cf9b32dc
e-cagan <144911316+e-cagan@users.noreply.github.com>
2024-01-30 19:47:37 +0300

    Add files via upload

---
+++ madlibs/madlibs.py
@@ -0,0 +1,8 @@
+adjactive = input("Adjactive: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adjactive}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb1} like you are {famous_person}!"
+
+print(madlib)
